fde9d6101468c3c2d245387198a3dfc3b97fbfb6
mushu <gipity@163.com>
2025-08-19 21:36:23 +0800

    Delete abondon directory
    删除无用代码

--- abondon/static/js/sidebar.js
+++
@@ -1,8 +0,0 @@
-// 使用fetch获取sidebar.html的内容
-fetch('./static/html/sidebar.html')
-    .then(response => response.text())
-    .then(html => {
-        // 将获取到的内容插入到侧边栏容器中
-        document.getElementById('sidebar-container').innerHTML = html;
-    })
-    .catch(error => console.error('加载侧边栏失败:', error));
